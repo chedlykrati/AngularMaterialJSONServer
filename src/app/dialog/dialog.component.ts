a788f5ededd09dc1ac4ef6bcc25afa71ff6b2c0a
chedlykrati <chedly.krati@esprit.tn>
2022-07-06 08:07:49 +0100

    angular material

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormBuilder , Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent implements OnInit {
+
+  freshnessList = ["Brand New" , "Second Hand" , "Refurbished"] 
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
